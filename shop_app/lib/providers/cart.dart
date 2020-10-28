d1eb7aaa33d27899d220a14d32fad9d8d4c908ba
RGB <ragabelsayed37@gmail.com>
2020-10-28 19:20:48 +0200

    Adding Snackbar & Undoing Add to cart Actions

--- shop_app/lib/providers/cart.dart
+++ shop_app/lib/providers/cart.dart
@@ -67,6 +67,25 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey((productId))) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+        productId,
+        (existingCarItem) => CartItem(
+            id: existingCarItem.id,
+            price: existingCarItem.price,
+            quantity: existingCarItem.quantity - 1,
+            title: existingCarItem.title),
+      );
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
+
   void clear() {
     _items = {};
     notifyListeners();
