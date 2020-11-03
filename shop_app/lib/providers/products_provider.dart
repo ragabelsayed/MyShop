b864507cfc399c51e954c12e6f8e816dbbf857cd
RGB <ragabelsayed37@gmail.com>
2020-11-03 21:00:26 +0200

    Adding http package & Sending POST Requests

--- shop_app/lib/providers/products_provider.dart
+++ shop_app/lib/providers/products_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'product.dart';
 
 class Products with ChangeNotifier {
@@ -64,6 +67,17 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    const url = 'https://my-flutter-shopapp.firebaseio.com/products.json';
+    http.post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    );
     final newProduct = Product(
         id: DateTime.now().toString(),
         title: product.title,
